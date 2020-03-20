432568b10dfdfebde0eb9d7ba43df1474fe52cb1
Robert Osfield <robert@openscenegraph.com>
2020-03-20 08:58:27 +0000

    Removed debug messages

--- applications/vsgconv/vsgconv.cpp
+++ applications/vsgconv/vsgconv.cpp
@@ -13,44 +13,37 @@
 
 int main(int argc, char** argv)
 {
-    // write out command line to aid debugging
-    for(int i=0; i<argc; ++i)
-    {
-        std::cout<<argv[i]<<" ";
-    }
-    std::cout<<std::endl;
-
     // set up defaults and read command line arguments to override them
     vsg::CommandLine arguments(&argc, argv);
-    auto outputFilename = arguments.value(std::string(), "-o");
-    auto pathFilename = arguments.value(std::string(),"-p");
 
     // read shaders
     vsg::Paths searchPaths = vsg::getEnvPaths("VSG_FILE_PATH");
-
-    using VsgObjects = std::vector<vsg::ref_ptr<vsg::Object>>;
-    VsgObjects vsgObjects;
-
 
     // ise the vsg::Options object to pass the ReaderWriter_all to use when reading files.
     auto options = vsg::Options::create();
     options->readerWriter = vsgXchange::ReaderWriter_all::create();
 
-    // read any input files
-    for (int i=1; i<argc; ++i)
+    if (argc <= 1)
     {
-        std::string filename = arguments[i];
+        std::cout<<"Usage:\n";
+        std::cout<<"    vsgconv input_filename output_filefilename\n";
+        std::cout<<"    vsgconv input_filename_1 input_filefilename_2 output_filefilename\n";
+        return 1;
+    }
 
-        auto before_vsg_load = std::chrono::steady_clock::now();
+    vsg::Path outputFilename = arguments[argc-1];
+
+    using VsgObjects = std::vector<vsg::ref_ptr<vsg::Object>>;
+    VsgObjects vsgObjects;
+
+    // read any input files
+    for (int i=1; i<argc-1; ++i)
+    {
+        vsg::Path filename = arguments[i];
 
         auto loaded_object = vsg::read(filename, options);
-
-        auto vsg_loadTime = std::chrono::duration<double, std::chrono::milliseconds::period>(std::chrono::steady_clock::now() - before_vsg_load).count();
-
         if (loaded_object)
         {
-            std::cout<<"LoadTime = "<<vsg_loadTime<<"ms"<<std::endl;
-
             vsgObjects.push_back(loaded_object);
             arguments.remove(i, 1);
             --i;
@@ -90,7 +83,6 @@ int main(int argc, char** argv)
     if (numImages==vsgObjects.size())
     {
         // all images
-        std::cout<<"All images"<<std::endl;
         vsg::ref_ptr<vsg::Node> vsg_scene;
 
 
@@ -125,8 +117,6 @@ int main(int argc, char** argv)
     else if (numShaders==vsgObjects.size())
     {
         // all shaders
-        std::cout<<"All shaders"<<std::endl;
-
         vsg::ShaderStages stagesToCompile;
         for(auto& object : vsgObjects)
         {
@@ -154,8 +144,6 @@ int main(int argc, char** argv)
     else if (numNodes==vsgObjects.size())
     {
         // all nodes
-        std::cout<<"All nodes"<<std::endl;
-
         vsg::ref_ptr<vsg::Node> vsg_scene;
         if (numNodes == 1) vsg_scene = dynamic_cast<vsg::Node*>(vsgObjects.front().get());
         else
@@ -175,7 +163,6 @@ int main(int argc, char** argv)
     }
     else
     {
-        std::cout<<"Mixed objects"<<std::endl;
         if (!outputFilename.empty())
         {
             if (vsgObjects.size()==1)
