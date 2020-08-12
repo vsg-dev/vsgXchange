b3faaf2f333fe8357a78477a2aed52d9522e9d0b
Robert Osfield <robert@openscenegraph.com>
2020-08-12 16:30:59 +0100

    Changed DescriptorImage usage to retain compatibility with VSG changes

--- src/osg/SceneBuilder.cpp
+++ src/osg/SceneBuilder.cpp
@@ -249,7 +249,7 @@ vsg::ref_ptr<vsg::DescriptorImage> SceneBuilderBase::convertToVsgTexture(const o
     vsg::ref_ptr<vsg::Sampler> sampler = vsg::Sampler::create();
     sampler->info() = convertToSamplerCreateInfo(osgtexture);
 
-    auto texture = vsg::DescriptorImage::create(vsg::SamplerImage{sampler, textureData}, 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
+    auto texture = vsg::DescriptorImage::create(sampler, textureData, 0, 0, VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER);
     texturesMap[osgtexture] = texture;
 
     return texture;
