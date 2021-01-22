06e9c97701f0d7f633da0e0dd738e037f2e16274
Andre Normann <andre@fam-normann.de>
2021-01-22 12:26:35 +0100

    - Added texture loading via stbi (png, jpg, psd, bmp, gif and hdr files) - Added mesh loading via assimp

---
+++ src/stbi/ReaderWriter_stbi.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vsg/io/ReaderWriter.h>
+#include <vsgXchange/Export.h>
+#include <unordered_set>
+
+namespace vsgXchange
+{
+
+class VSGXCHANGE_DECLSPEC ReaderWriter_stbi : public vsg::Inherit<vsg::ReaderWriter, ReaderWriter_stbi>
+    {
+public:
+    ReaderWriter_stbi();
+
+    vsg::ref_ptr<vsg::Object> read(const vsg::Path& filename, vsg::ref_ptr<const vsg::Options> options = {}) const override;
+    vsg::ref_ptr<vsg::Object> read(std::istream& fin, vsg::ref_ptr<const vsg::Options> options = {}) const override;
+
+private:
+    std::unordered_set<std::string> _supportedExtensions;
+};
+
+} // namespace vsgXchange
