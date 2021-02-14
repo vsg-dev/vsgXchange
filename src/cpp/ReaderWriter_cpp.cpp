9008b9449dab0185dddd37e10c578b8bd90c4c6b
Robert Osfield <robert@openscenegraph.com>
2021-02-14 11:34:35 +0000

    Restructured so that console output only gets written when the write is operating.

--- src/cpp/ReaderWriter_cpp.cpp
+++ src/cpp/ReaderWriter_cpp.cpp
@@ -13,33 +13,31 @@ ReaderWriter_cpp::ReaderWriter_cpp()
 
 bool ReaderWriter_cpp::write(const vsg::Object* object, const vsg::Path& filename, vsg::ref_ptr<const vsg::Options> /*options*/) const
 {
+    auto ext = vsg::lowerCaseFileExtension(filename);
+    if (ext != "cpp") return false;
+
     std::cout << "ReaderWriter_cpp::write(" << object->className() << ", " << filename << ")" << std::endl;
 
-    auto ext = vsg::fileExtension(filename);
-    if (ext == "cpp")
-    {
-        std::string funcname = vsg::simpleFilename(filename);
+    std::string funcname = vsg::simpleFilename(filename);
 
-        std::ostringstream str;
+    std::ostringstream str;
 
-        vsg::ReaderWriter_vsg io;
-        io.write(object, str);
+    vsg::ReaderWriter_vsg io;
+    io.write(object, str);
 
-        std::ofstream fout(filename);
-        fout << "#include <vsg/io/ReaderWriter_vsg.h>\n";
-        fout << "static auto " << funcname << " = []() {";
-        fout << "std::istringstream str(\n";
-        write(fout, str.str());
-        fout << ");\n";
+    std::ofstream fout(filename);
+    fout << "#include <vsg/io/ReaderWriter_vsg.h>\n";
+    fout << "static auto " << funcname << " = []() {";
+    fout << "std::istringstream str(\n";
+    write(fout, str.str());
+    fout << ");\n";
 
-        fout << "vsg::ReaderWriter_vsg io;\n";
-        fout << "return io.read_cast<" << object->className() << ">(str);\n";
-        fout << "};\n";
-        fout.close();
+    fout << "vsg::ReaderWriter_vsg io;\n";
+    fout << "return io.read_cast<" << object->className() << ">(str);\n";
+    fout << "};\n";
+    fout.close();
 
-        return true;
-    }
-    return false;
+    return true;
 }
 
 void ReaderWriter_cpp::write(std::ostream& out, const std::string& str) const
