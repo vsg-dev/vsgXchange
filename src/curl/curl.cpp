c0ba70eca7c8ef2ff7d0f17524a89bb4655fe708
Robert Osfield <robert@openscenegraph.com>
2022-02-21 15:08:56 +0000

    Added missing include

--- src/curl/curl.cpp
+++ src/curl/curl.cpp
@@ -16,6 +16,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <curl/curl.h>
 
 #include <fstream>
+#include <sstream>
 #include <iostream>
 
 using namespace vsgXchange;
