baa79eda934a353f2c74c76adbbc630286b8ab06
Robert Osfield <robert@openscenegraph.com>
2022-05-11 11:07:15 +0100

    Added includes to handle move of vsg::Path stream operators to stream.h

--- src/dds/dds.cpp
+++ src/dds/dds.cpp
@@ -13,6 +13,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <vsgXchange/images.h>
 
 #include <vsg/io/FileSystem.h>
+#include <vsg/io/stream.h>
 
 #include <cstring>
 
