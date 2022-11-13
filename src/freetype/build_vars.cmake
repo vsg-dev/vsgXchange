53ad453c78f3a77c2b64b1c34d1f349a1ab99e0b
Robert Osfield <robert@openscenegraph.com>
2022-11-13 10:06:13 +0000

    Added support for modern freetype config support where available.

--- src/freetype/build_vars.cmake
+++ src/freetype/build_vars.cmake
@@ -1,7 +1,13 @@
 # add freetype if available
-find_package(Freetype)
+# first check for modern freetype config file
+find_package(freetype CONFIG QUIET)
 
-if(FREETYPE_FOUND)
+# if freetype config not found fallback to using the original FindFreetype.cmake module
+if (NOT freetype_FOUND)
+    find_package(Freetype)
+endif()
+
+if(freetype_FOUND OR FREETYPE_FOUND)
     OPTION(vsgXchange_freetype "Freetype support provided" ON)
 endif()
 
@@ -9,12 +15,23 @@ if(${vsgXchange_freetype})
     set(SOURCES ${SOURCES}
         freetype/freetype.cpp
     )
-    set(EXTRA_INCLUDES ${EXTRA_INCLUDES} ${FREETYPE_INCLUDE_DIRS})
-    set(EXTRA_LIBRARIES ${EXTRA_LIBRARIES} ${FREETYPE_LIBRARIES})
+
     set(EXTRA_DEFINES ${EXTRA_DEFINES} USE_FREETYPE)
 
-    if(NOT BUILD_SHARED_LIBS)
-        set(FIND_DEPENDENCY ${FIND_DEPENDENCY} "find_dependency(Freetype)")
+    if (freetype_FOUND)
+        # new freetype config usage
+        set(EXTRA_LIBRARIES Freetype::Freetype)
+        if(NOT BUILD_SHARED_LIBS)
+            set(FIND_DEPENDENCY ${FIND_DEPENDENCY} "find_dependency(freetype)")
+        endif()
+    else()
+        # old Freetype module usage
+        set(EXTRA_INCLUDES ${EXTRA_INCLUDES} ${FREETYPE_INCLUDE_DIRS})
+        set(EXTRA_LIBRARIES ${EXTRA_LIBRARIES} ${FREETYPE_LIBRARIES})
+
+        if(NOT BUILD_SHARED_LIBS)
+            set(FIND_DEPENDENCY ${FIND_DEPENDENCY} "find_dependency(Freetype)")
+        endif()
     endif()
 else()
     set(SOURCES ${SOURCES}
